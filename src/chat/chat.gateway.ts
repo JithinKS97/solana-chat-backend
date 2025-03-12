6c86d734011466a12804e91b690c5d7cee64db67
Jithin KS <jithinks@Jithins-MacBook-Pro-2.local>
2025-03-12 23:35:01 +0800

    add chat

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,11 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway(80, { transports: ['websocket'], cors: true })
 export class ChatGateway {
     @SubscribeMessage('message')
-    handleMessage(@MessageBody() message: string) {
-        console.log(message);
+    handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket) {
+       console.log(message);
+       client.emit('message', "Here is the response");
     }
 }
