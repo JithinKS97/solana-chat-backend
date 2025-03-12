dda5d42af9a1879f588c00729d317809ffc1de7b
Jithin KS <jithinks@Jithins-MacBook-Pro-2.local>
2025-03-12 23:01:23 +0800

    add chat reception

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,6 +1,6 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 
-@WebSocketGateway(80, { transports: ['websocket'] })
+@WebSocketGateway(80, { transports: ['websocket'], cors: true })
 export class ChatGateway {
     @SubscribeMessage('message')
     handleMessage(@MessageBody() message: string) {
